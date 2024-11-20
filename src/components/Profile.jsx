2f214d45f9571fcfc3b8586cb8e8bb8f337e290e
masud467 <masudpfrnt@gmail.com>
2024-11-20 23:30:06 +0600

    update:add profile dropdown.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,40 +6,43 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Button } from "./ui/button";
-import { FaCircleUser } from "react-icons/fa6";
-// import { IoSettings } from 'react-icons/io5';
-import { IoMdArrowDropdown } from "react-icons/io";
+import { Button } from './ui/button';
+import { FaCircleUser } from 'react-icons/fa6';
+import { IoSettings } from 'react-icons/io5';
+import { BiDonateHeart } from 'react-icons/bi';
+import { LuDownload } from "react-icons/lu";
 
 const Profile = () => {
   return (
-    <div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            className="overflow-hidden rounded-full ml-auto flex"
-          >
-            <FaCircleUser className="size-7" />
-            
-          </Button>
+    <div className='flex '>
+      <div className="flex items-center gap-6 ml-auto">
           
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="w-52">
-          <DropdownMenuLabel>Support Us</DropdownMenuLabel>
-          <DropdownMenuLabel>Download Dua App</DropdownMenuLabel>
-          <DropdownMenuLabel>Privacy Policy</DropdownMenuLabel>
-          <DropdownMenuLabel>Thanks & Credits</DropdownMenuLabel>
-          <DropdownMenuLabel>About Us</DropdownMenuLabel>
-          <DropdownMenuLabel>Copyright Warning</DropdownMenuLabel>
-          <DropdownMenuLabel>Our Other Projects</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-        </DropdownMenuContent>
-      </DropdownMenu>
-      {/* <IoSettings className="text-green-700 text-3xl" /> */}
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="overflow-hidden rounded-full w-12 h-12"
+                  >
+                    <FaCircleUser className="size-8" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-52">
+                  <DropdownMenuLabel className='flex gap-3'> <BiDonateHeart size={20} className='text-green-600' />Support Us</DropdownMenuLabel>
+                  <DropdownMenuLabel className='flex gap-3'> <LuDownload className='text-green-600'/>Download Dua App</DropdownMenuLabel>
+                  <DropdownMenuLabel>Privacy Policy</DropdownMenuLabel>
+                  <DropdownMenuLabel>Thanks & Credits</DropdownMenuLabel>
+                  <DropdownMenuLabel>About Us</DropdownMenuLabel>
+                  <DropdownMenuLabel>Copyright Warning</DropdownMenuLabel>
+                  <DropdownMenuLabel>Our Other Projects</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <IoSettings className="text-green-700 text-2xl" />
+            
+          </div>
     </div>
   );
-};
+}
 
 export default Profile;
---
+++ src/components/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div className='bg-white h-[840px] rounded-lg shadow-lg p-4'>
+      <h1 className='text-center pt-5 font-semibold text-xl'>Settings</h1>
+    </div>
+  );
+}
+
+export default Settings;
