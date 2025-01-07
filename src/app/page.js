10b8b48f77c39b5553156595a6d4f241706e4ccb
masud467 <masudpfrnt@gmail.com>
2025-01-07 12:35:06 +0600

    remove sidebar and category section from this page.

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Categories from "@/components/Categories";
 import Main from "@/components/Main";
 import Profile from "@/components/Profile";
 import Sidebar from "@/components/Sidebar";
+import { useState } from "react";
 // import { useState } from "react";
 
 export default function Home() {
@@ -11,24 +12,23 @@ export default function Home() {
   // const handleCategorySelect = (category) => {
   //   setSelectedCategory(category);
   // };
-  return (
-    <div className="bg-[#EBEEF2] h-screen py-10 overflow-hidden">
-      <div className="grid grid-cols-12 gap-4 mx-8 ">
-        <div className="col-span-1 ">
-          <Sidebar></Sidebar>
-        </div>
 
-        <div className="col-span-3 ">
+  return (
+    <div>
+      {/* <div className="col-span-1 ">
+          <Sidebar></Sidebar>
+        </div> */}
+
+      {/* <div className="col-span-3 ">
           <h1 className="text-xl font-semibold pb-5">Dua Pages</h1>
-          <Categories ></Categories>
-        </div>
-        <div className="col-span-8">
-          <Main ></Main>
-        </div>
-        {/* <div className="col-span-2">
+          <Categories></Categories>
+        </div> */}
+      <div>
+        <Main></Main>
+      </div>
+      {/* <div className="col-span-2">
           <Profile></Profile>
         </div> */}
-      </div>
     </div>
   );
 }
